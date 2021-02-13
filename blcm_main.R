a528b8db8ef5c4321a8a043e3fb15da9b11ac0f2
Paolo Eusebi <paoloeusebi@gmail.com>
2021-02-13 01:49:57 +0100

    plot code added

--- blcm_main.R
+++ blcm_main.R
@@ -1,58 +1,50 @@
 #Test1: PCR
-#Test2: IgGorM, IgG only, IgM only
+#Test2: IgG or IgM, IgG only, IgM only
 
 getwd()
 
-# Libraries
-#install.packages("runjags", "rjags")
+# libraries
+# install.packages("runjags", "rjags")
+library(tidyverse)
+library(readxl)
 library(runjags)
 library(rjags)
 testjags()
 
-# Directory
 
 # Model
-source("covid_r1_dep_III.R")
-#other choice: blcm_2test_mpop_prior_I.R
-#other choice: blcm_2test_mpop_prior_II.R
-#other choice: blcm_2test_mpop_prior_III.R
-#other choice: blcm_2test_mpop_dep_prior_I.R
-#other choice: blcm_2test_mpop_dep_prior_II.R
-#other choice: blcm_2test_mpop_dep_prior_III.R
+source("covid_r1_ind_I.R")
+# source("covid_r1_ind_II.R")
+# source("covid_r1_ind_III.R")
+# source("covid_r1_dep_I.R")
+# source("covid_r1_dep_II.R")
+# source("covid_r1_dep_III.R")
 
 # Data
-library(tidyverse)
-library(readxl)
+
 df <- read_excel('data.xls')
 
 df2 <- df %>%
-  filter(Ab %in% "IgGM") %>% #other choices: IgM, IgG, IgG/M
+  filter(Ab %in% "IgGM") %>% # other choices: IgM, IgG
   select(Week, Author, `PCR+/Ab+`, `PCR+/Ab-`, `PCR-/Ab+`, `PCR-/Ab-`) %>%
   group_by(Week, Author) %>%
-  summarise_all(funs(sum)) 
-
+  summarise_all(funs(sum)) %>%
+  ungroup()
 df2
 
 y <- df2 %>%
-  ungroup() %>%
   select(`PCR+/Ab+`, `PCR+/Ab-`, `PCR-/Ab+`, `PCR-/Ab-`) %>%
   as.matrix()
-
 y
 
-m = length(unique(df$Author))
-n = apply(y, 1, sum)
+m = length(unique(df$Author)) # number of studies
+n = apply(y, 1, sum) #sample size for each study
 
 
-#initials
-
-inits1 = list(".RNG.name" ="base::Mersenne-Twister",
-              ".RNG.seed" = 100022)
-inits2 = list(".RNG.name" ="base::Mersenne-Twister",
-              ".RNG.seed" = 300022)
-inits3 = list(".RNG.name" ="base::Mersenne-Twister",
-              ".RNG.seed" = 500022)
-
+# initial values
+inits1 = list(".RNG.name" ="base::Mersenne-Twister", ".RNG.seed" = 100022)
+inits2 = list(".RNG.name" ="base::Mersenne-Twister", ".RNG.seed" = 300022)
+inits3 = list(".RNG.name" ="base::Mersenne-Twister", ".RNG.seed" = 500022)
 
 # Running
 results <- run.jags(model, 
@@ -61,3 +53,7 @@ results <- run.jags(model,
                     burnin = 10000,
                     sample = 60000)
 print(results)
+plot(results,
+     vars = list("Se", "Sp"),
+     layout = c(4, 6),
+     plot.type = c("trace", "histogram", "autocorr"))
